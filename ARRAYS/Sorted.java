500210f4cecdda632174a4ec6514f2f13677a758
Priyanka-saw <priyankasaw51@gmail.com>
2025-06-19 16:12:54 +0530

    sorted array

--- ARRAYS/Sorted.java
+++ ARRAYS/Sorted.java
@@ -1,16 +1,19 @@
 package ARRAYS;
 
+import java.util.Arrays;
+
 public class Sorted {
     
     public static void main(String[] args) {
         int[] arr = {1, 2, 3 ,4 ,5};
         System.out.println(sortedArr(arr));
+        System.out.println(Arrays.toString(arr));
 
     }
     public static boolean  sortedArr(int[] arr){
         for (int i = 1; i < arr.length; i++) {
             if(arr[i] >= arr[i-1]){
-                System.out.println(i);
+                
             } else {
                 return false;
             }
