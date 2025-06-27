b5c2456d8a1945d2f87857a984ee976c69372136
Priyanka-saw <priyankasaw51@gmail.com>
2025-06-27 22:52:29 +0530

    concate

---
+++ PrimaryQ/ConcatenateTwoSting.java
@@ -0,0 +1,10 @@
+package PrimaryQ;
+
+public class ConcatenateTwoSting {
+    public static void main(String[] args){
+        String a = "priyanka";
+        String b = "kumari";
+
+        System.out.println(a.concat(b));
+    }
+}
