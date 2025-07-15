e53e3128642a97e11f35930dfd3d09dffdc5503f
Priyanka-saw <priyankasaw51@gmail.com>
2025-07-15 18:54:58 +0530

    updated

---
+++ KK_playlist/firstClass.java
@@ -0,0 +1,72 @@
+package KK_playlist;
+
+import java.util.Scanner;
+
+public class firstClass {
+    public static void main(String[] args){
+        System.out.println("hello world");
+
+        // taking input from the user
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println(sc.nextLine());
+        // System.out.println("enter your roll number : ");
+        // int rollno = sc.nextInt();
+        // System.out.println("your roll numbr is : " + rollno);
+        
+        // data types
+        // int rollno = 23;
+        // char letter = 'p';
+        // double largeDecimalNumber = 232332.2323;
+        // float marks = 23.32f;
+        // long largeIntger = 23232322l;
+        // boolean  check = true;
+
+        // int a = 23_000_000;
+        // System.out.println(a);
+
+        // sum
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("enter number");
+        // float num1 = sc.nextFloat();
+        // float num2 = sc.nextFloat();
+        // float sum = num1 + num2;
+        // System.out.println("sum of the number is " + sum);
+
+        // type casting
+        // int num = (int)(34.34f);
+        // System.out.println(num);
+
+        // java follow all the unicode principle
+
+
+        // condition
+        // while
+        // int count = 1;
+        // while(count != 5){
+        //     System.out.println(count);
+        //     count++;
+        // }
+
+        // for
+        // for(int i = 1; i<=10; i++){
+        //     System.out.println(i);
+        // }
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter value");
+        float tempC = sc.nextFloat();
+        float tempF = (tempC * 9/5) + 32;
+        System.out.println(tempF);
+
+
+
+
+        // String is not primitive
+        // String name = "priyanka";
+
+
+
+    }
+}
+ 
