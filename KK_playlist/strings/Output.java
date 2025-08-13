fce669033b5f4227e39d58f96e4e41a96b574b26
Priyanka-saw <priyankasaw51@gmail.com>
2025-08-13 15:21:39 +0530

    update

---
+++ KK_playlist/strings/Output.java
@@ -0,0 +1,20 @@
+package KK_playlist.strings;
+
+import java.util.Arrays;
+
+public class Output {
+    public static void main(String[] args){
+
+        System.out.println("priyanka");
+        System.out.println(23);
+
+        System.out.println(Arrays.toString(new int[]{1, 2, 3, 4, 5}));
+        
+
+        String value = null;
+        System.out.println(value);
+
+        
+
+    }
+}
