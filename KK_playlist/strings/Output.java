1ad18e8c10066906b37ed77cddcb262fc16f3d1d
Priyanka-saw <priyankasaw51@gmail.com>
2025-08-13 17:29:57 +0530

    alphabet

--- KK_playlist/strings/Output.java
+++ KK_playlist/strings/Output.java
@@ -1,20 +1,37 @@
 package KK_playlist.strings;
 
 import java.util.Arrays;
+// import java.util.ArrayList
 
 public class Output {
     public static void main(String[] args){
 
-        System.out.println("priyanka");
-        System.out.println(23);
+        // System.out.println("priyanka");
+        // System.out.println(23);
 
-        System.out.println(Arrays.toString(new int[]{1, 2, 3, 4, 5}));
+        // System.out.println(Arrays.toString(new int[]{1, 2, 3, 4, 5}));
         
+        // String value = null;
+        // System.out.println(value);
 
-        String value = null;
-        System.out.println(value);
 
+        // pretty printing
+
+        // float a = 23.34f;
+        // System.out.printf("formatted number is %.2 f" , a);
+
+        // System.out.printf("Pie: %.3f" , Math.PI);
+
+        System.out.println('a' + 'b');   //it convert it into ascaii value 
+        System.out.println("a" + "b");  //string value not converted into ascii value
+        System.out.println('a' + 3);   
+        System.out.println("a" + 1);
+        System.out.println('a' + 1);
+
+        // System.out.println("priyanka" + new ArrayList<>());  //priyanka[]
         
+        // System.out.println("priyanka" + new Integer(23));  //priyanka23
 
     }
 }
+        
