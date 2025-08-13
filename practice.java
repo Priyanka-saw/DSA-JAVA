d4b7be391d6a089a57797f222b8d707a39ed63a9
Priyanka-saw <priyankasaw51@gmail.com>
2025-08-13 15:02:15 +0530

    practice

--- practice.java
+++ practice.java
@@ -1,35 +1,38 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class practice {
+
     public static void main(String[] args) {
 
-        int arr[] = {11, 12, 13, 14, 15};
-    //     System.out.println("Maximum value is : " + MaxVal(arr));
-    // }
-    // public static int MaxVal(int[] arr){
-    //     int max = -1, secmax = -1;
-    //     for (int i = 0; i < arr.length; i++) {
-    //         if(arr[i] > max){
-    //             max = arr[i];
-    //         }
-    //     }
-    //     for (int i = 0; i < arr.length; i++) {
-    //         if(arr[i] > secmax && arr[i] != max){
-    //             secmax = arr[i];
-    //         } 
-    //     }
-    //     return  secmax;
+        // factorial
+        // int fact = 1;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter number");
+        // int user = sc.nextInt();
 
-    System.out.println("sorted value : " + sortedval(arr));
-        System.out.println(Arrays.toString(arr));
-    }
-    public static boolean sortedval(int[] arr){
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i] >= arr[i - 1]) {
+        // for(int i = 1; i <= user; i++){
+        //     fact = fact * i;
+        // }
+        // System.out.println(fact);
 
-            } else{
-                return false;
-            }
+
+        // fibonacci
+        // 0, 1, 1, 2, 3, 5, 8
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter");
+        
+        int user = sc.nextInt();
+        int first = 0;
+        int sec = 1;
+
+        for (int i = 1; i < user; i++) {
+            System.out.println(first);
+
+            int temp = first + sec;
+            first = sec;
+            sec = temp;
         }
-        return true;
+
+
     }
 }
